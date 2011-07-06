9a6c27357614465d1331127409f8b72efbb092d5
Joshy Cyriac <j.cyriac@gmail.com>
2011-07-06 21:28:15 +0200

    changed to canvas, removed raphael

--- javascript/dragon.js
+++ javascript/dragon.js
@@ -7,8 +7,11 @@ window.onload = function() {
    }); 
 };
 
-
 function resetPaper() {
+    canvas = document.getElementById('board');
+    ctx = canvas.getContext('2d');
+    ctx.strokeStyle = "blue";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     path_size = parseInt($('#size').val());
     var width = 800;
     var height = 400;
@@ -16,8 +19,6 @@ function resetPaper() {
     y0 = height / 2;
     x1 = x0;
     y1 = y0 - path_size;
-    $('#holder').empty();
-    r = Raphael("holder", width, height);
     turn = new Turn();
     orientation = 'N';
 }
@@ -25,68 +26,55 @@ function resetPaper() {
 
 function Turn() {
     this.index = 0;
+    this.turn = function(direction) {
+	x0 = x1;
+	y0 = y1;
+	if (direction == 'right') {
+	    if (orientation == 'N') {
+		orientation = 'O';
+		x1 = x1 + path_size;
+	    } else if (orientation == 'O') {
+		orientation = 'S';
+		y1 = y1 + path_size;
+	    } else if (orientation == 'S') {
+		orientation = 'W';
+		x1 = x1 - path_size;
+	    } else if (orientation == 'W') {
+		orientation = 'N';
+		y1 = y1 - path_size;
+	    }
+	    return;
+	}
+	if (direction == 'left') {
+	    if (orientation == 'N') {
+		orientation = 'W';
+		x1 = x1 - path_size;
+	    } else if (orientation == 'W') {
+		orientation = 'S';
+		y1 = y1 + path_size;
+	    } else if (orientation == 'S') {
+		orientation = 'O';
+		x1 = x1 + path_size;
+	    } else if (orientation == 'O') {
+		orientation = 'N';
+		y1 = y1 - path_size;
+	    }
+	    return;
+	}
+
+    }
 }
-
-Turn.prototype.look = function(grade) {
-    if (grade > 0) {
-	this.index++;
-	if (this.index > 3) {
-	    this.index = 0;
-	}
-    } else {
-	this.index--;
-	if (this.index < 0) {
-	    this.index = 3;
-	}
-    }
-};
-
-Turn.prototype.turn = function(direction) {
-    x0 = x1;
-    y0 = y1;
-    if (direction == 'right') {
-	this.look(1);
-	if (orientation == 'N') {
-	    orientation = 'O';
-	    x1 = x1 + path_size;
-	} else if (orientation == 'O') {
-	    orientation = 'S';
-	    y1 = y1 + path_size;
-	} else if (orientation == 'S') {
-	    orientation = 'W';
-	    x1 = x1 - path_size;
-	} else if (orientation == 'W') {
-	    orientation = 'N';
-	    y1 = y1 - path_size;
-	}
-	return;
-    }
-    if (direction == 'left') {
-	this.look(-1);
-	if (orientation == 'N') {
-	    orientation = 'W';
-	    x1 = x1 - path_size;
-	} else if (orientation == 'W') {
-	    orientation = 'S';
-	    y1 = y1 + path_size;
-	} else if (orientation == 'S') {
-	    orientation = 'O';
-	    x1 = x1 + path_size;
-	} else if (orientation == 'O') {
-	    orientation = 'N';
-	    y1 = y1 - path_size;
-	}
-	return;
-    }
-
-};
 
 
 function dragon(code) {
     if (code == 0) {
-	r.path("M " + x0 + " " + y0 + "L" + x1 + " " + y1).attr({stroke:"blue"});
-    }
-    else {
+	ctx.beginPath();
+	ctx.moveTo(x0, y0);
+	ctx.lineTo(x1,y1);
+	ctx.closePath();
+	ctx.stroke();
+//	r.path("M " + x0 + " " + y0 + "L" + x1 + " " + y1).attr({stroke:"blue"});
+    } else {
 	dragon(code-1);
 	turn.turn('right');
 	dragon_inverse(code-1);
@@ -94,10 +82,14 @@ function dragon(code) {
 }
 
 function dragon_inverse(code) {
-    if (code == 0)
-	r.path("M " + x0 + " " + y0 + "L" + x1 + " " + y1).attr({stroke:"blue"});
-
-    else {
+    if (code == 0) {
+	ctx.beginPath();
+	ctx.moveTo(x0, y0);
+	ctx.lineTo(x1,y1);
+	ctx.closePath();
+	ctx.stroke();
+//	r.path("M " + x0 + " " + y0 + "L" + x1 + " " + y1).attr({stroke:"blue"});
+    } else {
 	dragon(code-1);
 	turn.turn('left');
 	dragon_inverse(code-1);
